3187b4b4d790bddba9b4c4f16dd30f788a91bf64
Kai Takami <kaitakami@protonmail.com>
2022-07-24 15:51:56 +0900

    Finish project 4, use setInterval, clearInterval methods

--- project-2/index.js
+++ project-2/index.js
@@ -5,20 +5,27 @@ modalInformation.forEach((modalInfo, index) => {
   modalInfo.addEventListener("click", () => {
     if (modalDescriptionEl[index].classList.contains("modal-hidden")) {
       modalDescriptionEl[index].classList.remove("modal-hidden");
-      document.querySelectorAll('.modal-icon')[index].classList.add('modal-icon-active')
+      document
+        .querySelectorAll(".modal-icon")
+        [index].classList.add("modal-icon-active");
       setTimeout(function () {
         modalDescriptionEl[index].classList.remove("modal-invisible");
         console.log("hello");
       }, 30);
     } else {
-      document.querySelectorAll('.modal-icon')[index].classList.remove('modal-icon-active')
+      document
+        .querySelectorAll(".modal-icon")
+        [index].classList.remove("modal-icon-active");
       modalDescriptionEl[index].classList.add("modal-invisible");
-      modalDescriptionEl[index].addEventListener("transitionend", (e) => {
-        modalDescriptionEl[index].classList.add("modal-hidden");
-        
-      }, {
-        once: true
-      });
+      modalDescriptionEl[index].addEventListener(
+        "transitionend",
+        (e) => {
+          modalDescriptionEl[index].classList.add("modal-hidden");
+        },
+        {
+          once: true,
+        }
+      );
     }
   });
 });
---
+++ project-3/index.js
@@ -0,0 +1,91 @@
+// Variables for buttons
+
+const startStopBtn = document.querySelector("#startStopBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+// Variables for time values
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// Variables for leading zero
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+// Variables for set interval & timer status
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+// Stop Watch Function
+
+const stopWatch = () => {
+  seconds++;
+
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  if (seconds < 10) {
+    leadingSeconds = "0" + seconds.toString();
+  } else {
+    leadingSeconds = seconds;
+  }
+
+  if (minutes < 10) {
+    leadingMinutes = "0" + minutes.toString();
+  } else {
+    leadingMinutes = minutes;
+  }
+
+  if (hours < 10) {
+    leadingHours = "0" + hours.toString();
+  } else {
+    leadingHours = hours;
+  }
+
+  let displayTimer = document.getElementById("timer");
+  displayTimer.textContent = `${leadingHours}:${leadingMinutes}:${leadingSeconds}`;
+};
+
+startStopBtn.addEventListener("click", function () {
+  if (timerStatus === "stopped") {
+    timerInterval = window.setInterval(stopWatch, 1000);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" id="pause" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+  </svg>`;
+    timerStatus = "started";
+  } else {
+    window.clearInterval(timerInterval);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" id="play" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+  </svg>`;
+    timerStatus = "stopped";
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  window.clearInterval(timerInterval);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  document.getElementById("timer").innerHTML = "00:00:00";
+  document.getElementById(
+    "startStopBtn"
+  ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" id="play" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+  </svg>`;
+});
